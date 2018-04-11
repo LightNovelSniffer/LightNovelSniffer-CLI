e1aee567c7fac76c1ca7fc242799e10767a88916
JoshuaArus <joshua.arus@gmail.com>
2018-04-11 09:31:05 +0200

    First version of CLI.
    Based on LightNovelSniffer state before splitting core&UI

---
+++ ConsoleTools.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using LightNovelSniffer.Config;
+using LightNovelSniffer.Libs;
+
+namespace LightNovelSniffer_CLI
+{
+    internal class ConsoleTools : IInput, IOutput
+    {
+        private bool isProgressOngoing = false;
+
+        private void CheckProgress()
+        {
+            if (isProgressOngoing)
+            {
+                Console.WriteLine("");
+                isProgressOngoing = false;
+            }
+        }
+
+        private void OutputString(string str)
+        {
+            Console.Write(DateTime.Now.ToString("HH:mm:ss") + " : " + str);
+        }
+
+        private string Indent(string text, int tab)
+        {
+            while (tab > 1)
+            {
+                text = "  " + text;
+                tab--;
+            }
+
+            return text;
+        }
+
+        public void Log(string text, int tab)
+        {
+            CheckProgress();
+            OutputString(Indent(text, tab) + "\r\n");
+        }
+
+        public void Progress(string text, int tab)
+        {
+            isProgressOngoing = true;
+            Console.Write("\r" + DateTime.Now.ToString("HH:mm:ss") + " : " + Indent(text, tab));
+        }
+
+        public bool Ask(string question)
+        {
+            string input = AskInformation(question + " O/n ");
+            return input != null && !input.ToUpper().Equals("N");
+        }
+
+        public string AskInformation(string question)
+        {
+            if (!Globale.INTERACTIVE_MODE)
+                return "";
+            CheckProgress();
+            OutputString(question);
+            return Console.ReadLine();
+        }
+
+        public string AskUrl(string question)
+        {
+            string url = AskInformation(question);
+
+            if (string.IsNullOrEmpty(url))
+                return null;
+
+            url = url.TrimEnd('/');
+
+            while (Regex.Match(url, @"\d$").Success)
+            {
+                url = url.Substring(0, url.Length - 1);
+            }
+
+            return url + "{0}";
+        }
+
+        public bool AskNegative(string question)
+        {
+            string input = AskInformation(question + " o/N ");
+            return !(string.IsNullOrEmpty(input) || input.ToUpper().Equals("N"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using LightNovelSniffer.Config;
+using LightNovelSniffer.Web;
+
+namespace LightNovelSniffer_CLI
+{
+    public static class Program
+    {
+        private static WebCrawler webCrawler;
+        private static ConsoleTools consoleTools;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                ConfigTools.InitConf();
+                consoleTools = new ConsoleTools();
+                webCrawler = new WebCrawler(consoleTools, consoleTools);
+            }
+            catch (ApplicationException e)
+            {
+                consoleTools.Log(e.Message, 1);
+                return;
+            }
+
+            if (!consoleTools.Ask("Utiliser le dossier de sortie \"" + Globale.OUTPUT_FOLDER + "\" ?"))
+            {
+                string folder = consoleTools.AskInformation("Merci de saisir le chemin du dossier racine à utiliser : ");
+                if (!string.IsNullOrEmpty(folder))
+                    Globale.OUTPUT_FOLDER = folder;
+            }
+
+            consoleTools.Log("Début du programme", 1);
+
+            foreach (LnParameters ln in Globale.LN_TO_RETRIEVE)
+            {
+                if (consoleTools.Ask(string.Format("Voulez-vous récupérer {0} ?", ln.name.ToUpper())))
+                    GetNovel(ln);
+            }
+
+            if (Globale.INTERACTIVE_MODE && consoleTools.AskNegative("Fin du traitement des Light Novel prédéfinies. Voulez vous en traiter d'autres ?"))
+            {
+                LnParameters ln;
+                do
+                {
+                    ln = BuildDynamicLn();
+                    GetNovel(ln);
+                } while (!string.IsNullOrEmpty(ln.name));
+
+            }
+
+            consoleTools.Log("Fin du programme", 1);
+            if (Globale.INTERACTIVE_MODE)
+                Console.ReadLine();
+        }
+
+        private static void GetNovel(LnParameters ln)
+        {
+            foreach (UrlParameter up in ln.urlParameters)
+            {
+                if (consoleTools.Ask(string.Format("Voulez-vous récupérer la version {0} ?", up.language)))
+                {
+                    if (string.IsNullOrEmpty(up.url))
+                        up.url = consoleTools.AskUrl(string.Format("Aucune URL renseignée pour {0} dans cette langue. Merci de saisir l'url d'un chapitre ici: ", ln.name));
+
+                    if (!string.IsNullOrEmpty(up.url))
+                        webCrawler.DownloadChapters(ln, up.language);
+                    else
+                        consoleTools.Log(string.Format("Pas d'URL pour la version {0}. Traitement arrêté", up.language), 1);
+                }
+            }
+        }
+
+        private static LnParameters BuildDynamicLn()
+        {
+            LnParameters ln = new LnParameters();
+
+            ln.name = consoleTools.AskInformation("Nom de la LN (vide pour arrêter) : ");
+            if (string.IsNullOrEmpty(ln.name))
+                return ln;
+            ln.urlCover = consoleTools.AskInformation("URL de l'image de cover : ");
+            string authors = consoleTools.AskInformation("Auteurs (séparés par des virgules) : ");
+            if (!string.IsNullOrEmpty(authors))
+                foreach (string author in authors.Split(','))
+                    ln.authors.Add(author.Trim());
+
+            do
+            {
+                UrlParameter up = new UrlParameter();
+
+                up.language = consoleTools.AskInformation("Entrez la langue de cette version (FR/EN/...) : ");
+                up.url = consoleTools.AskInformation("Entrez l'url d'un des chapitres : ");
+                int.TryParse(consoleTools.AskInformation("A quel chapitre voulez vous commencer (si vide, 1) : "), out up.firstChapterNumber);
+                int.TryParse(consoleTools.AskInformation("A quel chapitre voulez vous vous arrêter (si vide, jusqu'au dernier paru) : "), out up.lastChapterNumber);
+
+                if (!string.IsNullOrEmpty(up.language) && !string.IsNullOrEmpty(up.url))
+                    ln.urlParameters.Add(up);
+                else
+                    consoleTools.Log("Pas d'URL ou de language pour cette version. Ajout impossible", 1);
+            } while (consoleTools.Ask("Voulez vous ajouter une autre version pour cette LN ?"));
+
+            return ln;
+        }
+    }
+}
